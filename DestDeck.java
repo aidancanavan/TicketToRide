2c8b142f925256d98f1f29c98c3a9bc1d4530b24
Paul Macfarlane <33580162+PaulMacSiena@users.noreply.github.com>
2019-03-29 14:30:31 -0400

    Add files via upload

---
+++ DestDeck.java
@@ -0,0 +1,18 @@
+import java.util.Deque;
+/**
+ * Represents a deck of Destination Ticket Objects
+ *
+ * @author Paul Macfarlane
+ * @version 1.0
+ */
+//////////////////////////////////////////////////////////////////////////////////////////
+public class DestDeck extends Deck
+{
+    /**
+     * Constructor for objects of class DestDeck
+     */
+    public DestDeck()    {
+        discards = null; //no deck for the discards of desitination tickets
+    }    
+  
+}
---
+++ TransDeck.java
@@ -0,0 +1,19 @@
+import java.util.Deque;
+/**
+ * Object representing a transportation card deck in ticketToRide
+ *
+ * @author Paul Macfarlane
+ * @version 1.0
+ */
+//////////////////////////////////////////////////////////////////////////////////////////
+public class TransDeck extends Deck
+{
+
+    /**
+     * Constructor for objects of class TransDeck
+     */ 
+    public TransDeck() {
+          
+    }
+   
+}
